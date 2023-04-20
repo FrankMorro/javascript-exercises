a46075b3658d7181ccf8c6aa4deee6f2bc026207
JRMartinez <30609352+FrankMorro@users.noreply.github.com>
2023-04-20 15:09:37 -0400

    Solution exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // A shorter option
+  return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
 
+  // Here is another way to get the same result, but a little more long.
+  /*
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+  */
 };
 
 // Do not edit below this line
