dfaf9ba747751f9f82acb0b6db1a2d05bcd89a29
JRMartinez <30609352+FrankMorro@users.noreply.github.com>
2023-04-20 11:42:08 -0400

    Solution using typeof for error

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
 
+  if (num1 < 0 || num2 < 0) return "ERROR";
+
+  let sumTotal = 0;
+
+  const arr = [num1, num2];
+
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+
+  let min = arr[0];
+  let max = arr[1];
+
+  // console.log(arr);
+
+  for (let i = min; i <= max; i++) {
+    sumTotal += i;
+  }
+
+  return sumTotal;
 };
 
 // Do not edit below this line
