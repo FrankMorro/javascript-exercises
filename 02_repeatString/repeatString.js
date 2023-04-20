519c274e5cb2a5ce88664175e641a0c6fdfb10ad
JRMartinez <30609352+FrankMorro@users.noreply.github.com>
2023-04-19 22:22:45 -0400

    Fixed test 02 using the repeat method of a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (text, count) {
+  if (count < 0) return "ERROR";
 
+  return text.repeat(count);
+
+  /*
+  // Could this code, which will throw the same result
+
+  let myText = "";
+  for (let index = 0; index < count; index++) {
+    myText += text;
+  }
+
+  return myText;
+  */
 };
 
 // Do not edit below this line
