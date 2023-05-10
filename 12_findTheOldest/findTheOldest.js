5f719d858424c97b66c2978d496adbc9e495dcf6
JRMartinez <30609352+FrankMorro@users.noreply.github.com>
2023-05-10 17:22:11 -0400

    Add solution exercise 12. Total madness

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const old = people.reduce((a, b) => {
+    let yearDay1 = new Date().getFullYear()
+    let yearDay2 = new Date().getFullYear()
 
-};
+    console.log(yearDay1, yearDay2)
+
+    if (a.yearOfDeath !== undefined) {
+      yearDay1 = a.yearOfDeath
+    }
+
+    if (b.yearOfDeath !== undefined) {
+      yearDay2 = b.yearOfDeath
+    }
+
+    return yearDay1 - a.yearOfBirth > yearDay2 - b.yearOfBirth ? a : b
+  })
+
+  return old
+}
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
