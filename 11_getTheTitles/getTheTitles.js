201306dda61f1f88b5f558c5fccf329f6577ba6b
JRMartinez <30609352+FrankMorro@users.noreply.github.com>
2023-05-10 11:57:52 -0400

    Add solution exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  titleBook = []
 
-};
+  for (let i = 0; i < books.length; i++) {
+    titleBook.push(books[i].title)
+  }
+
+  return titleBook
+}
 
 // Do not edit below this line
-module.exports = getTheTitles;
+module.exports = getTheTitles
