70e11628a641d7d47eebbe6a715acbe9dea1f87c
JRMartinez <30609352+FrankMorro@users.noreply.github.com>
2023-05-10 10:33:03 -0400

    Add solution exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (frase) {
+  //We remove the blank spaces
+  let newFrase = frase.replace(/\s/g, '')
 
-};
+  // We remove the special characters. We only allow letters and numbers
+  newFrase = newFrase.replace(/[^a-zA-Z0-9 ]/g, '')
+  newFrase = newFrase.toLowerCase() // We convert to lowercase
+
+  let revertir = newFrase.split('').reverse().join('').toLowerCase()
+
+  return newFrase === revertir ? true : false
+
+  // if (newFrase === revertir) return true
+
+  // return false
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
